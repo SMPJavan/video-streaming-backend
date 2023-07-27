67f589008af63e3710f6322e583b0652a9395add
simon_pebltec <simon@pebltec.co.uk>
2023-07-27 10:07:17 +0100

    Moving event bridge factory to common module

--- apps/micronaut-common/src/main/java/ssp/video/stream/micronaut/common/factories/EventBridgeClientFactory.java
+++ apps/micronaut-common/src/main/java/ssp/video/stream/micronaut/common/factories/EventBridgeClientFactory.java
@@ -1,19 +1,4 @@
-/*
- * Copyright 2017-2020 original authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-package ssp.video.stream.configuration;
+package ssp.video.stream.micronaut.common.factories;
 
 import io.micronaut.aws.sdk.v2.service.AWSServiceConfiguration;
 import io.micronaut.aws.sdk.v2.service.AwsClientFactory;
@@ -23,6 +8,7 @@ import io.micronaut.context.annotation.Factory;
 import io.micronaut.context.annotation.Requires;
 import io.micronaut.core.annotation.Nullable;
 import jakarta.inject.Named;
+import jakarta.inject.Singleton;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProviderChain;
 import software.amazon.awssdk.http.SdkHttpClient;
 import software.amazon.awssdk.http.async.SdkAsyncHttpClient;
@@ -31,7 +17,6 @@ import software.amazon.awssdk.services.eventbridge.EventBridgeAsyncClient;
 import software.amazon.awssdk.services.eventbridge.EventBridgeAsyncClientBuilder;
 import software.amazon.awssdk.services.eventbridge.EventBridgeClient;
 import software.amazon.awssdk.services.eventbridge.EventBridgeClientBuilder;
-import jakarta.inject.Singleton;
 
 /**
  * Factory that creates an EventBridge client.
---
+++ apps/micronaut-common/src/test/java/ssp/video/stream/micronaut/common/factories/EventBridgeClientFactoryTest.java
@@ -0,0 +1,95 @@
+package ssp.video.stream.micronaut.common.factories;
+
+import io.micronaut.aws.sdk.v2.service.AWSServiceConfiguration;
+import io.micronaut.aws.ua.UserAgentProvider;
+import org.junit.jupiter.api.Test;
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProviderChain;
+import software.amazon.awssdk.http.SdkHttpClient;
+import software.amazon.awssdk.http.async.SdkAsyncHttpClient;
+import software.amazon.awssdk.regions.providers.AwsRegionProviderChain;
+import software.amazon.awssdk.services.eventbridge.EventBridgeAsyncClient;
+import software.amazon.awssdk.services.eventbridge.EventBridgeAsyncClientBuilder;
+import software.amazon.awssdk.services.eventbridge.EventBridgeClient;
+import software.amazon.awssdk.services.eventbridge.EventBridgeClientBuilder;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.mock;
+
+public class EventBridgeClientFactoryTest {
+
+    @Test
+    void should_create_sync_builder() {
+        AwsCredentialsProviderChain credentialsProvider = mock(AwsCredentialsProviderChain.class);
+        AwsRegionProviderChain regionProvider = mock(AwsRegionProviderChain.class);
+        UserAgentProvider userAgentProvider = mock(UserAgentProvider.class);
+        AWSServiceConfiguration awsServiceConfiguration = mock(AWSServiceConfiguration.class);
+        EventBridgeClientFactory factory = new EventBridgeClientFactory(credentialsProvider, regionProvider, userAgentProvider, awsServiceConfiguration);
+        EventBridgeClientBuilder builder = factory.createSyncBuilder();
+        assertNotNull(builder);
+    }
+
+    @Test
+    void should_create_async_builder() {
+        AwsCredentialsProviderChain credentialsProvider = mock(AwsCredentialsProviderChain.class);
+        AwsRegionProviderChain regionProvider = mock(AwsRegionProviderChain.class);
+        UserAgentProvider userAgentProvider = mock(UserAgentProvider.class);
+        AWSServiceConfiguration awsServiceConfiguration = mock(AWSServiceConfiguration.class);
+        EventBridgeClientFactory factory = new EventBridgeClientFactory(credentialsProvider, regionProvider, userAgentProvider, awsServiceConfiguration);
+        EventBridgeAsyncClientBuilder builder = factory.createAsyncBuilder();
+        assertNotNull(builder);
+    }
+
+    @Test
+    void should_sync_builder() {
+        AwsCredentialsProviderChain credentialsProvider = mock(AwsCredentialsProviderChain.class);
+        AwsRegionProviderChain regionProvider = mock(AwsRegionProviderChain.class);
+        UserAgentProvider userAgentProvider = mock(UserAgentProvider.class);
+        AWSServiceConfiguration awsServiceConfiguration = mock(AWSServiceConfiguration.class);
+        SdkHttpClient httpClient = mock(SdkHttpClient.class);
+        EventBridgeClientFactory factory = new EventBridgeClientFactory(credentialsProvider, regionProvider, userAgentProvider, awsServiceConfiguration);
+        EventBridgeClientBuilder builder = factory.syncBuilder(httpClient);
+        assertNotNull(builder);
+    }
+
+    @Test
+    void should_sync_client() {
+        AwsCredentialsProviderChain credentialsProvider = mock(AwsCredentialsProviderChain.class);
+        AwsRegionProviderChain regionProvider = mock(AwsRegionProviderChain.class);
+        UserAgentProvider userAgentProvider = mock(UserAgentProvider.class);
+        AWSServiceConfiguration awsServiceConfiguration = mock(AWSServiceConfiguration.class);
+        EventBridgeClientBuilder builder = mock(EventBridgeClientBuilder.class);
+        EventBridgeClient client = mock(EventBridgeClient.class);
+        doReturn(client).when(builder).build();
+        EventBridgeClientFactory factory = new EventBridgeClientFactory(credentialsProvider, regionProvider, userAgentProvider, awsServiceConfiguration);
+        EventBridgeClient result = factory.syncClient(builder);
+        assertEquals(client, result);
+    }
+
+    @Test
+    void should_async_builder() {
+        AwsCredentialsProviderChain credentialsProvider = mock(AwsCredentialsProviderChain.class);
+        AwsRegionProviderChain regionProvider = mock(AwsRegionProviderChain.class);
+        UserAgentProvider userAgentProvider = mock(UserAgentProvider.class);
+        AWSServiceConfiguration awsServiceConfiguration = mock(AWSServiceConfiguration.class);
+        SdkAsyncHttpClient httpClient = mock(SdkAsyncHttpClient.class);
+        EventBridgeClientFactory factory = new EventBridgeClientFactory(credentialsProvider, regionProvider, userAgentProvider, awsServiceConfiguration);
+        EventBridgeAsyncClientBuilder builder = factory.asyncBuilder(httpClient);
+        assertNotNull(builder);
+    }
+
+    @Test
+    void should_async_client() {
+        AwsCredentialsProviderChain credentialsProvider = mock(AwsCredentialsProviderChain.class);
+        AwsRegionProviderChain regionProvider = mock(AwsRegionProviderChain.class);
+        UserAgentProvider userAgentProvider = mock(UserAgentProvider.class);
+        AWSServiceConfiguration awsServiceConfiguration = mock(AWSServiceConfiguration.class);
+        EventBridgeAsyncClientBuilder builder = mock(EventBridgeAsyncClientBuilder.class);
+        EventBridgeAsyncClient client = mock(EventBridgeAsyncClient.class);
+        doReturn(client).when(builder).build();
+        EventBridgeClientFactory factory = new EventBridgeClientFactory(credentialsProvider, regionProvider, userAgentProvider, awsServiceConfiguration);
+        EventBridgeAsyncClient result = factory.asyncClient(builder);
+        assertEquals(client, result);
+    }
+}
